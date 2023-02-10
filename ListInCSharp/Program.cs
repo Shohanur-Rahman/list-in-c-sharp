e979643e7a0dd7d5297ce8cff5bd3415d0cda59c
shohanur rahman <shohanur.rahman57@gmail.com>
2023-02-10 22:40:37 +0600

    update

--- ListInCSharp/Program.cs
+++ ListInCSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ListInCSharp
 {
@@ -6,7 +8,21 @@ namespace ListInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> names = new List<string>()
+            {
+                "Rafiq Ahmed",
+                "Riad",
+                "Tamim",
+                "Rafiq Islam"
+            };
+
+            var rafiq = names.Where(x => x.Contains("Rafiq"));
+
+            foreach(var name in rafiq)
+            {
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
